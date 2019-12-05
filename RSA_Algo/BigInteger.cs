94c8bc8ffa69519a413070fc7d7d8f80d5762b51
A7med Morgan <49093543+AhmedShreifMorgan@users.noreply.github.com>
2019-12-05 18:06:24 +0200

    Add&sub Fn
    Add and sub function added

--- RSA_Algo/BigInteger.cs
+++ RSA_Algo/BigInteger.cs
@@ -19,32 +19,159 @@ namespace RSA_Algo
            int [] int_arr = new int[ch_arr.Length]; // O(1)
             for (int i = 0; i < int_arr.Length; i++) // O(N)
             {
-                int_arr[i] = Convert.ToInt32(ch_arr[i]);
+                //int_arr[i] = Convert.ToInt32(ch_arr[i
+                string s= ch_arr[i].ToString();
+               int_arr[i] = Int32.Parse(s);
+
             }
             return int_arr;
         }
         // must arr1 be >= arr2
-        public static int[] Add(int[] arr1, int[] arr2)  
+        //public static int[] Add(int[] arr1, int[] arr2)
+        //{
+        //    int size;
+        //    int[] R;
+        //    int r=0;
+        //    int sub;
+        //    bool carry = false;
+        // //   if (arr1.Length > arr2.Length)
+        // //   {
+        //        R = new int[arr1.Length + 1];
+        //        size = arr1.Length;
+        //        sub = arr1.Length - arr2.Length;
+        //    //   }
+        //    //else {
+        //    //    R = new int[arr2.Length + 1];
+        //    //    size = arr2.Length;
+        //    //    sub = arr1.Length - arr2.Length;
+        //    //}
+        //    for (int i = size - 1; i >= 0; i--)
+        //    {
+
+        //        if (carry) { r += 1; carry = false; }
+        //        if (i - 1 == -1) { r += arr1[i]; }
+        //        else
+        //        {
+        //            r += arr1[i] + arr2[i - sub];
+
+        //        }
+        //        if (r > 9) carry = true;
+        //        r = r % 10;
+        //        R[i + 1] = r;
+
+        //        r = 0;
+
+        //    }
+        //    return R;
+        //}
+
+        public static int[] ADD(int[] arr1, int[] arr2)
         {
-            int[] result = new int[arr1.Length];
-            for (int i = 0; i < arr1.Length; i++) // O(N)
+            int size;
+            int[] R, Demo;
+            int r = 0;
+            int sub;
+            bool carry = false;
+            if (arr1.Length > arr2.Length)
             {
-                if (arr2.Length > i)
+                size = arr1.Length;
+                Demo = new int[size];
+                sub = arr1.Length - arr2.Length;
+                for (int i = size - 1; i >= 0; i--)
                 {
-                    result[i] = arr1[i] + arr2[i];
+                    if (i - sub != -1)
+                        Demo[i] = arr2[i - sub];
+                    else break;
                 }
-                else {
-                    result[i] = arr1[i];
-                }
+                R = new int[arr1.Length + 1];
             }
-            return result;
+            else
+            {
+                size = arr2.Length;
+                Demo = new int[size];
+                sub = arr2.Length - arr1.Length;
+                for (int i = size - 1; i >= 0; i--)
+                {
+                    if (i - sub != -1)
+                        Demo[i] = arr1[i - sub];
+                    else break;
+                }
+                R = new int[arr2.Length + 1];
+            }
+            for (int i = size - 1; i >= 0; i--)
+            {
+                if (carry) { r += 1; carry = false; }
+                if (arr1.Length > arr2.Length)
+                r += arr1[i] + Demo[i];
+                else
+                    r += Demo[i] +arr2[i];
+
+                if (r > 9) carry = true;
+            r = r % 10;
+            R[i + 1] = r;
+
+            r = 0;
+        }
+            if (carry) R[0] = 1;
+
+            return R;
         }
 
+        public static int[] SUB(int[] arr1, int[] arr2)
+        {
+            int size;
+            int[] R, Demo;
+            int r = 0;
+            int sub;
+            int carry = 0;
+            if (arr1.Length > arr2.Length)
+            {
+                size = arr1.Length;
+                Demo = new int[size];
+                sub = arr1.Length - arr2.Length;
+                for (int i = size - 1; i >= 0; i--)
+                {
+                    if (i - sub != -1)
+                        Demo[i] = arr2[i - sub];
+                    else break;
+                }
+                R = new int[arr1.Length + 1];
+            }
+            else
+            {
+                size = arr2.Length;
+                Demo = new int[size];
+                sub = arr2.Length - arr1.Length;
+                for (int i = size - 1; i >= 0; i--)
+                {
+                    if (i - sub != -1)
+                        Demo[i] = arr1[i - sub];
+                    else break;
+                }
+                R = new int[arr2.Length + 1];
+            }
+            BigInteger.Display(Demo);
+            for (int i = size - 1; i >= 0; i--)
+            {
+               // if (carry==1) { r += 1; carry = 0; }
+                if (arr1.Length > arr2.Length)
+                    r = arr1[i] - Demo[i] -carry;
+                else
+                    r = Demo[i] - arr2[i] -carry;
+
+                if (r < 0) { r += 10; carry = 1; }
+                else carry = 0;
+               // r = r % 10;
+                R[i + 1] = r;
+                r = 0;
+            }
+            return R;
+        }
         public static void Display(int[] Num)
         {
             foreach (int n in Num)
             {
-                Console.Write(n + "\t");
+                Console.Write(n);
             }
             Console.WriteLine();
         }
@@ -52,8 +179,9 @@ namespace RSA_Algo
         {
             foreach (int n in pub_arr)
             {
-                Console.Write(n + "\t");
+                Console.Write(n);
             }
+            Console.WriteLine();
             Console.WriteLine();
         }
 
--- RSA_Algo/Program.cs
+++ RSA_Algo/Program.cs
@@ -12,18 +12,26 @@ namespace RSA_Algo
         {
 
             BigInteger big1 = new BigInteger();
-           // BigInteger big2 = new BigInteger();
 
-            big1.text= Console.ReadLine();
-           // big2.text = Console.ReadLine();
 
-            big1.pub_arr = BigInteger.convert(big1.text.ToCharArray());
-         //   big2.pub_arr = BigInteger.convert(big2.text.ToCharArray());
+            string s1 = "1242468682113840180763850624340867898830637122775434796573473991466933960634085363735808574823872230131509619596260435331836866582052453403290874467870290043697815376935298110895904163169442926407335197820883439691250684070655002714487685888";
+            string s2 = "417545487977981564551173186152102228498479694796653351848954185997611243506278579601980980219484255929864359810447039931";
+           // int s = (int)s1[0] - '0';
+            int[] arr1 = BigInteger.convert(s1.ToCharArray());
+            int[] arr2 = BigInteger.convert(s2.ToCharArray());
+            BigInteger.Display(arr1);
+            BigInteger.Display(arr2);
 
-            big1.Display();
-          //  big2.Display();
+            big1.pub_arr = BigInteger.SUB(arr1,arr2);
+            // BigInteger big2 = new BigInteger();
 
-          //  BigInteger.Display(BigInteger.Add(big1.pub_arr, big2.pub_arr));
+            //   big1.pub_arr = BigInteger.convert(big1.text.ToCharArray());
+            //   big2.pub_arr = BigInteger.convert(big2.text.ToCharArray());
+
+             big1.Display();
+            //  big2.Display();
+
+            //  BigInteger.Display(BigInteger.Add(big1.pub_arr, big2.pub_arr));
         }
     }
 }
